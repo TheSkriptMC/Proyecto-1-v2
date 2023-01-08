950b2b90bd0b26525bab7a5920dd0fd5ef83c1d6
TheSkriptMC <theskriptmc@gmail.com>
2023-01-08 13:22:45 -0500

    Final

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
 var button1 = document.querySelector(".btn-encriptar");
 var button2 = document.querySelector(".btn-desencriptar");
 var button3 = document.querySelector(".btn-copiar");
+var button4 = document.querySelector(".btn-borrar-input");
+var button5 = document.querySelector(".btn-borrar-content");
 var imagen = document.querySelector(".container-img");
 var texto1 = document.querySelector(".container-h3");
 var texto2 = document.querySelector(".container-p");
 var content = document.querySelector(".desencriptado");
+var input = document.querySelector(".area");
 
-// var button4 = document.querySelector("#borrar-1");
-// var button5 = document.querySelector("#borrar-2");
+
+
+
 
 
 button1.onclick = encriptar;
 button2.onclick = desencriptar;
 button3.onclick = copiar;
+button4.onclick = borrarInput;
+
+
+
+
 
 function hide() {
     imagen.classList.add("hide");
@@ -28,39 +37,77 @@ function show() {
 
 
 
+
+
 function encriptar() {
-    hide();
-    var frase = document.querySelector(".area").value.toLowerCase();
-
-    var textoEncriptado = frase.replace(/e/img, "enter");
-    textoEncriptado = textoEncriptado.replace(/o/img, "ober");
-    textoEncriptado = textoEncriptado.replace(/i/img, "imes");
-    textoEncriptado = textoEncriptado.replace(/a/img, "ai");
-    textoEncriptado = textoEncriptado.replace(/u/img, "ufat");
-
-    document.querySelector(".desencriptado").innerHTML = textoEncriptado;
     
-    
+
+    var frase = input.value.toLowerCase();
+    if (frase == "") {
+        alert("No hay nada para encriptar");
+        return;
+    }
+    else {
+
+        hide();
+        var textoEncriptado = frase.replace(/e/img, "enter");
+        textoEncriptado = textoEncriptado.replace(/o/img, "ober");
+        textoEncriptado = textoEncriptado.replace(/i/img, "imes");
+        textoEncriptado = textoEncriptado.replace(/a/img, "ai");
+        textoEncriptado = textoEncriptado.replace(/u/img, "ufat");
+
+        content.innerHTML = textoEncriptado;
+        
+    }
 }
 
 function desencriptar() {
-    hide();
-    var frase = document.querySelector(".area").value.toLowerCase();
+    
+    
+    var frase = input.value.toLowerCase();
 
-    var textoEncriptado = frase.replace(/enter/img, "e");
-    textoEncriptado = textoEncriptado.replace(/ober/img, "o");
-    textoEncriptado = textoEncriptado.replace(/imes/img, "i");
-    textoEncriptado = textoEncriptado.replace(/ai/img, "a");
-    textoEncriptado = textoEncriptado.replace(/ufat/img, "u");
+    if (frase == "") {
+        alert("No hay nada para encriptar");
+        return;
+    }
 
-    document.querySelector(".desencriptado").innerHTML = textoEncriptado;
+    else {
+        hide();
+        var textoEncriptado = frase.replace(/enter/img, "e");
+        textoEncriptado = textoEncriptado.replace(/ober/img, "o");
+        textoEncriptado = textoEncriptado.replace(/imes/img, "i");
+        textoEncriptado = textoEncriptado.replace(/ai/img, "a");
+        textoEncriptado = textoEncriptado.replace(/ufat/img, "u");
+
+        content.innerHTML = textoEncriptado;
+        
+    }
+}
+
+
+
+function borrarInput() {
+    input.value = "";
+}
+
+function borrarContent() {
+    content.value = "";
 }
 
 function copiar() {
-    var content = document.querySelector(".desencriptado");
-    content.select();
-    document.execCommand("copy");
-    
+    if (input.value == "") {
+        alert("No hay nada para copiar");
+        return;
+    }
+    else {
+        content.select();
+        document.execCommand("copy");
+
+        input.focus();
+        borrarInput();
+        show();
+        
+    }
 }
 
 // function borrar1() {
